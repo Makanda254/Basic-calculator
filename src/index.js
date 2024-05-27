f244507eb307caea6b8684433c91664130eafb92
Victor <victormakanda254@gmail.com>
2024-05-28 01:22:06 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const form=document.createElement('form')
     form.innerHTML=`
     <label for='firstnum'> Enter first number: </label>
-    <input id='firstnum' placeholder='Enter first number..' required />
+    <input type='number' id='firstnum' placeholder='Enter first number..' required />
     <label for='operators'> Choose operator: </label>
     <select id='operators' name='operators'>
          <option value='+'>+</option>
@@ -13,14 +13,14 @@ document.addEventListener('DOMContentLoaded', () => {
          <option value='*'>*</option>
     </select>
     <label for='secondnum'> Enter second number: </label>
-    <input id='secondnum' placeholder='Enter second number..' required />
-    <input type='submit' id='calculateBtn' value='Calculate'>
+    <input type='number' id='secondnum' placeholder='Enter second number..' required />
+    <input type='submit' id='calculateBtn' value='Calculate' />
     `
 
     formcalculator.appendChild(form);
-    const calculateBtn = document.querySelector('#calculateBtn');
+    //const calculateBtn = document.querySelector('#calculateBtn');
 
-    calculateBtn.addEventListener('click',(e)=>{
+    form.addEventListener('submit',(e)=>{
         e.preventDefault();
         handleCalculation();
         form.reset()
@@ -34,6 +34,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const secondNum = parseInt(document.querySelector('#secondnum').value)
 
         const operator = document.querySelector('#operators').value
+
 
         if (operator == '+'){
 
@@ -67,7 +68,7 @@ document.addEventListener('DOMContentLoaded', () => {
         answerContainer.removeChild(answerContainer.firstChild)
         const divContainer = document.createElement('div')
         divContainer.innerHTML=`
-        <h1>The answer is: ${answer}</h1>
+        <h2 style="text-align: center;">The result is: ${answer}</h2>
         `
         answerContainer.appendChild(divContainer)
 
